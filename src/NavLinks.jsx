fe5a0b7d8b31128598647ca13c4331f2838f00ff
Tibor <tibor736@hotmail.com>
2023-07-18 20:17:28 +0300

    navlinks css

---
+++ src/NavLinks.jsx
@@ -0,0 +1,13 @@
+import { useGlobalContext } from "./Context";
+import sublinks from "./data";
+
+const NavLinks = () => {
+	const {} = useGlobalContext();
+	return <div className="nav-links">
+      {sublinks.map((item)=>{
+         const {pageId, page} = item;
+         return <button key={pageId} className="nav-link">{page}</button>
+      })}
+   </div>;
+};
+export default NavLinks;
